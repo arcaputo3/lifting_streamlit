69e0563980d6b8a2b86235c9d1745f712260c526
Rich Caputo <rcaputo3@outlook.com>
2022-01-15 08:29:02 -0500

    Add volume calculation

--- src/analysis.py
+++ src/analysis.py
@@ -3,6 +3,15 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from typing import List, Dict
+
+def exercise_volume_map(data: List[Dict[str, int]]) -> List[int]:
+    return [
+        max(d.get('weight', 0), 1) * d.get('reps', 0)
+        for d in data
+    ]
+
+
 with open('processed_data/workouts.json', 'r') as file:
     workout_json_data = json.load(file)
 
@@ -10,10 +19,22 @@ all_exercises = pd.concat([pd.DataFrame(entry['exercises']) for entry in workout
 
 if __name__ == "__main__":
     x = 25
+    volume = all_exercises['sets'].apply(exercise_volume_map).apply(sum).to_frame().set_index(all_exercises['name'])
+    volume = volume.reset_index().groupby('name').sum()
     top_exercises = all_exercises.groupby('name')['sets'].count()
+    print(volume)
+    print(top_exercises)
+
     top_exercises.sort_values().iloc[-x:-1].plot(figsize=(10, 8), kind='barh')
     plt.xlabel('Frequency')
     plt.ylabel('Exercise')
     plt.title(f'Top {x} Exercises by Workout Frequency')
     plt.tight_layout()
-    plt.savefig(f'top_{x}_exercises.png')
+    plt.savefig(f'processed_data/top_{x}_exercises_frequency.png')
+
+    volume.sort_values(by='sets').iloc[-x:].plot(figsize=(10, 8), kind='barh')
+    plt.xlabel('Volume (lbs)')
+    plt.ylabel('Exercise')
+    plt.title(f'Top {x} Exercises by Volume')
+    plt.tight_layout()
+    plt.savefig(f'processed_data/top_{x}_exercises_volume.png')
